2642473b4669a4af466e11ac6df8d3d4af3ac7e4
James Schultz <ninjames101@gmail.com>
2017-07-17 17:04:51 -0600

    going to push and copy and try to deploy

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,11 +1,35 @@
 <template>
-  <div id="message">
+
+
+      <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <v-card>
+            <v-toolbar class="cyan" dark>
+              <v-toolbar-title>Say What You Feel</v-toolbar-title>
+            </v-toolbar>
+            <v-list two-line>
+              <template v-for="item in items">
+                <v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+                <v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+                <v-list-tile avatar v-else v-bind:key="item.title" href="javascript:;" download target="_blank">
+                  <v-list-tile-avatar>
+                    <img v-bind:src="item.avatar"></v-list-tile-avatar>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </template>
+            </v-list>
+          </v-card>
+        </v-flex>
+      </v-layout>
 
 
 
 
 
-  </div>
 </template>
 
 <script>
@@ -13,6 +37,7 @@ export default {
 name: 'message',
 data () {
   return {
+    items:[{title:'james', header:'Hey graduation on the 28th'},{title:'peaches', header:'meow', subtitle:'yoyoyo'},{title:'jules', header:'sup', subtitle:'o oo'}]
 
   }
 }
@@ -21,12 +46,7 @@ data () {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#message{
-  height: 200px;
-  width: 100vw;
-  background-color: #F2EFEA;
 
-}
 
 
 
